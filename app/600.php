be425d0f24c635fa45c5c6efbe85dbddbc2dcca9
CHRISTIAN E. ZELAYA SOSA <CEZS-SYSTEM@MacBook-Pro-de-CHRISTIAN.local>
2019-01-19 19:10:10 -0300

    bug 600.php

--- app/600.php
+++ app/600.php
@@ -3,33 +3,43 @@
         require __DIR__.'/../src/connect.php';
         
 		$sql                        = "SELECT
-		d.DOMFIC_COD		AS		estado_categoria_subcategoria_codigo,
-		d.DOMFIC_NOM		AS		estado_categoria_subcategoria_nombre,
-		c.DOMFIC_COD		AS		subcategoria_codigo, 
-		c.DOMFIC_NOM		AS		subcategoria_nombre, 
-		b.DOMFIC_COD		AS		categoria_codigo,
-		b.DOMFIC_NOM		AS		categoria_nombre, 
-		a.DOMCYS_COD		AS		categoria_subcategoria_codigo, 
-		a.DOMCYS_OBS		AS		categoria_subcategoria_observacion
+		d.DOMFIC_COD		AS		estado_tipo_subtipo_codigo,
+		d.DOMFIC_NOM		AS		estado_tipo_subtipo_nombre,
+		c.DOMFIC_COD		AS		subtipo_codigo, 
+		c.DOMFIC_NOM		AS		subtipo_nombre,
+        c.DOMFIC_VAL		AS		subtipo_valor,
+        c.DOMFIC_OBS		AS		subtipo_observacion, 
+		b.DOMFIC_COD		AS		tipo_codigo,
+		b.DOMFIC_NOM		AS		tipo_nombre,
+        b.DOMFIC_VAL		AS		tipo_valor,
+        b.DOMFIC_OBS		AS		tipo_observacion, 
+		a.DOMTYS_COD		AS		tipo_subtipo_codigo,
+        a.DOMTYS_VAL		AS		tipo_subtipo_valor, 
+		a.DOMTYS_OBS		AS		tipo_subtipo_observacion
 		
-		FROM DOMCYS a
-		INNER JOIN DOMFIC b ON a.DOMFIC_CAC = b.DOMFIC_COD
-		INNER JOIN DOMFIC c ON a.DOMFIC_SUC = c.DOMFIC_COD
-		INNER JOIN DOMFIC d ON a.DOMFIC_COD = d.DOMFIC_COD
+		FROM DOMTYS a
+		INNER JOIN DOMFIC b ON a.DOMTYS_TIC = b.DOMFIC_COD
+		INNER JOIN DOMFIC c ON a.DOMTYS_SUC = c.DOMFIC_COD
+		INNER JOIN DOMFIC d ON a.DOMTYS_ESC = d.DOMFIC_COD
 		
 		ORDER BY b.DOMFIC_NOM, c.DOMFIC_NOM";
 		
         if ($query = $mysqli->query($sql)) {
             while($row = $query->fetch_assoc()) {				
                 $detalle			= array(
-					'estado_categoria_subcategoria_codigo'					=> $row['estado_categoria_subcategoria_codigo'],
-					'estado_categoria_subcategoria_nombre'					=> $row['estado_categoria_subcategoria_nombre'],
-					'subcategoria_codigo'				                    => $row['subcategoria_codigo'],
-					'subcategoria_nombre'				                    => $row['subcategoria_nombre'],
-					'categoria_codigo'					                    => $row['categoria_codigo'],
-					'categoria_nombre'					                    => $row['categoria_nombre'],
-					'categoria_subcategoria_codigo'			                => $row['categoria_subcategoria_codigo'],
-                    'categoria_subcategoria_observacion'	                => $row['categoria_subcategoria_observacion']
+					'estado_tipo_subtipo_codigo'                => $row['estado_tipo_subtipo_codigo'],
+                    'estado_tipo_subtipo_nombre'		        => $row['estado_tipo_subtipo_nombre'],
+					'subtipo_codigo'				            => $row['subtipo_codigo'],
+                    'subtipo_nombre'				            => $row['subtipo_nombre'],
+                    'subtipo_valor'	                            => $row['subtipo_valor'],
+                    'subtipo_observacion'				        => $row['subtipo_observacion'],
+					'tipo_codigo'					            => $row['tipo_codigo'],
+                    'tipo_nombre'					            => $row['tipo_nombre'],
+                    'tipo_valor'			                    => $row['tipo_valor'],
+                    'tipo_observacion'			                => $row['tipo_observacion'],
+					'tipo_subtipo_codigo'			            => $row['tipo_subtipo_codigo'],
+                    'tipo_subtipo_valor'	                    => $row['tipo_subtipo_valor'],
+                    'tipo_subtipo_observacion'			        => $row['tipo_subtipo_observacion']
 				);	
                 $result[]           = $detalle;
             }
@@ -54,19 +64,24 @@
         
 		$val00                      = $request->getAttribute('codigo');
 		$sql                        = "SELECT
-		d.DOMFIC_COD		AS		estado_categoria_subcategoria_codigo,
-		d.DOMFIC_NOM		AS		estado_categoria_subcategoria_nombre,
-		c.DOMFIC_COD		AS		subcategoria_codigo, 
-		c.DOMFIC_NOM		AS		subcategoria_nombre, 
-		b.DOMFIC_COD		AS		categoria_codigo,
-		b.DOMFIC_NOM		AS		categoria_nombre, 
-		a.DOMCYS_COD		AS		categoria_subcategoria_codigo, 
-		a.DOMCYS_OBS		AS		categoria_subcategoria_observacion
+		d.DOMFIC_COD		AS		estado_tipo_subtipo_codigo,
+		d.DOMFIC_NOM		AS		estado_tipo_subtipo_nombre,
+		c.DOMFIC_COD		AS		subtipo_codigo, 
+		c.DOMFIC_NOM		AS		subtipo_nombre,
+        c.DOMFIC_VAL		AS		subtipo_valor,
+        c.DOMFIC_OBS		AS		subtipo_observacion, 
+		b.DOMFIC_COD		AS		tipo_codigo,
+		b.DOMFIC_NOM		AS		tipo_nombre,
+        b.DOMFIC_VAL		AS		tipo_valor,
+        b.DOMFIC_OBS		AS		tipo_observacion, 
+		a.DOMTYS_COD		AS		tipo_subtipo_codigo,
+        a.DOMTYS_VAL		AS		tipo_subtipo_valor, 
+		a.DOMTYS_OBS		AS		tipo_subtipo_observacion
 		
-		FROM DOMCYS a
-		INNER JOIN DOMFIC b ON a.DOMFIC_CAC = b.DOMFIC_COD
-		INNER JOIN DOMFIC c ON a.DOMFIC_SUC = c.DOMFIC_COD
-		INNER JOIN DOMFIC d ON a.DOMFIC_COD = d.DOMFIC_COD
+		FROM DOMTYS a
+		INNER JOIN DOMFIC b ON a.DOMTYS_TIC = b.DOMFIC_COD
+		INNER JOIN DOMFIC c ON a.DOMTYS_SUC = c.DOMFIC_COD
+		INNER JOIN DOMFIC d ON a.DOMTYS_ESC = d.DOMFIC_COD
 		
 		WHERE a.DOMCYS_COD = '$val00'
 		ORDER BY b.DOMFIC_NOM, c.DOMFIC_NOM";
@@ -74,14 +89,81 @@
         if ($query = $mysqli->query($sql)) {
             while($row = $query->fetch_assoc()) {				
                 $detalle			= array(
-					'estado_categoria_subcategoria_codigo'					=> $row['estado_categoria_subcategoria_codigo'],
-					'estado_categoria_subcategoria_nombre'					=> $row['estado_categoria_subcategoria_nombre'],
-					'subcategoria_codigo'				                    => $row['subcategoria_codigo'],
-					'subcategoria_nombre'				                    => $row['subcategoria_nombre'],
-					'categoria_codigo'					                    => $row['categoria_codigo'],
-					'categoria_nombre'					                    => $row['categoria_nombre'],
-					'categoria_subcategoria_codigo'			                => $row['categoria_subcategoria_codigo'],
-                    'categoria_subcategoria_observacion'	                => $row['categoria_subcategoria_observacion']
+					'estado_tipo_subtipo_codigo'                => $row['estado_tipo_subtipo_codigo'],
+                    'estado_tipo_subtipo_nombre'		        => $row['estado_tipo_subtipo_nombre'],
+					'subtipo_codigo'				            => $row['subtipo_codigo'],
+                    'subtipo_nombre'				            => $row['subtipo_nombre'],
+                    'subtipo_valor'	                            => $row['subtipo_valor'],
+                    'subtipo_observacion'				        => $row['subtipo_observacion'],
+					'tipo_codigo'					            => $row['tipo_codigo'],
+                    'tipo_nombre'					            => $row['tipo_nombre'],
+                    'tipo_valor'			                    => $row['tipo_valor'],
+                    'tipo_observacion'			                => $row['tipo_observacion'],
+					'tipo_subtipo_codigo'			            => $row['tipo_subtipo_codigo'],
+                    'tipo_subtipo_valor'	                    => $row['tipo_subtipo_valor'],
+                    'tipo_subtipo_observacion'			        => $row['tipo_subtipo_observacion']
+				);	
+                $result[]           = $detalle;
+            }
+			$query->free();
+        }
+        
+        $mysqli->close();
+        
+        if (isset($result)){
+            header("Content-Type: application/json; charset=utf-8");
+            $json                   = json_encode(array('code' => 200, 'status' => 'ok', 'message' => 'Consulta con exito', 'data' => $result), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+        } else {
+            header("Content-Type: application/json; charset=utf-8");
+            $json                   = json_encode(array('code' => 204, 'status' => 'ok', 'message' => 'No hay registros', 'data' => 'null'), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+        }
+        
+        return $json;
+    });
+
+    $app->get('/api/v1/600/dominio/{codigo}', function($request) {
+        require __DIR__.'/../src/connect.php';
+        
+		$val00                      = $request->getAttribute('codigo');
+		$sql                        = "SELECT
+		d.DOMFIC_COD		AS		estado_tipo_subtipo_codigo,
+		d.DOMFIC_NOM		AS		estado_tipo_subtipo_nombre,
+		c.DOMFIC_COD		AS		subtipo_codigo, 
+		c.DOMFIC_NOM		AS		subtipo_nombre,
+        c.DOMFIC_VAL		AS		subtipo_valor,
+        c.DOMFIC_OBS		AS		subtipo_observacion, 
+		b.DOMFIC_COD		AS		tipo_codigo,
+		b.DOMFIC_NOM		AS		tipo_nombre,
+        b.DOMFIC_VAL		AS		tipo_valor,
+        b.DOMFIC_OBS		AS		tipo_observacion, 
+		a.DOMTYS_COD		AS		tipo_subtipo_codigo,
+        a.DOMTYS_VAL		AS		tipo_subtipo_valor, 
+		a.DOMTYS_OBS		AS		tipo_subtipo_observacion
+		
+		FROM DOMTYS a
+		INNER JOIN DOMFIC b ON a.DOMTYS_TIC = b.DOMFIC_COD
+		INNER JOIN DOMFIC c ON a.DOMTYS_SUC = c.DOMFIC_COD
+		INNER JOIN DOMFIC d ON a.DOMTYS_ESC = d.DOMFIC_COD
+		
+		WHERE a.DOMTYS_VAL = '$val00'
+		ORDER BY b.DOMFIC_NOM, c.DOMFIC_NOM";
+		
+        if ($query = $mysqli->query($sql)) {
+            while($row = $query->fetch_assoc()) {				
+                $detalle			= array(
+					'estado_tipo_subtipo_codigo'                => $row['estado_tipo_subtipo_codigo'],
+                    'estado_tipo_subtipo_nombre'		        => $row['estado_tipo_subtipo_nombre'],
+					'subtipo_codigo'				            => $row['subtipo_codigo'],
+                    'subtipo_nombre'				            => $row['subtipo_nombre'],
+                    'subtipo_valor'	                            => $row['subtipo_valor'],
+                    'subtipo_observacion'				        => $row['subtipo_observacion'],
+					'tipo_codigo'					            => $row['tipo_codigo'],
+                    'tipo_nombre'					            => $row['tipo_nombre'],
+                    'tipo_valor'			                    => $row['tipo_valor'],
+                    'tipo_observacion'			                => $row['tipo_observacion'],
+					'tipo_subtipo_codigo'			            => $row['tipo_subtipo_codigo'],
+                    'tipo_subtipo_valor'	                    => $row['tipo_subtipo_valor'],
+                    'tipo_subtipo_observacion'			        => $row['tipo_subtipo_observacion']
 				);	
                 $result[]           = $detalle;
             }
@@ -104,13 +186,14 @@
 	$app->post('/api/v1/600', function($request) {
         require __DIR__.'/../src/connect.php';
 
-		$val01                      = $request->getParsedBody()['estado_categoria_subcategoria_codigo'];
-        $val02                      = $request->getParsedBody()['categoria_codigo'];
-        $val03                      = $request->getParsedBody()['subcategoria_codigo'];
-		$val04                      = $request->getParsedBody()['categoria_subcategoria_observacion'];
+		$val01                      = $request->getParsedBody()['estado_tipo_subtipo_codigo'];
+        $val02                      = $request->getParsedBody()['tipo_codigo'];
+        $val03                      = $request->getParsedBody()['subtipo_codigo'];
+        $val04                      = $request->getParsedBody()['tipo_subtipo_valor'];
+        $val05                      = $request->getParsedBody()['tipo_subtipo_observacion'];
         
         if (isset($val01) && isset($val02) && isset($val03)) {
-            $sql                    = "INSERT INTO DOMCYS (DOMCYS_ECS, DOMCYS_CAC, DOMCYS_SUC, DOMCYS_OBS) VALUES ('$val01', '$val02', '$val03', '".$val04."')";
+            $sql                    = "INSERT INTO DOMTYS (DOMTYS_ESC, DOMTYS_TIC, DOMTYS_SUC, DOMTYS_VAL, DOMTYS_OBS) VALUES ('$val01', '$val02', '$val03', '".$val04."', '".$val05."')";
             if ($mysqli->query($sql) === TRUE) {
                 header("Content-Type: application/json; charset=utf-8");
                 $json               = json_encode(array('code' => 200, 'status' => 'ok', 'message' => 'Se inserto con exito', 'codigo' => $mysqli->insert_id), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
@@ -132,13 +215,14 @@
         require __DIR__.'/../src/connect.php';
         
         $val00                      = $request->getAttribute('codigo');
-		$val01                      = $request->getParsedBody()['estado_categoria_subcategoria_codigo'];
-        $val02                      = $request->getParsedBody()['categoria_codigo'];
-        $val03                      = $request->getParsedBody()['subcategoria_codigo'];
-		$val04                      = $request->getParsedBody()['categoria_subcategoria_observacion'];
+		$val01                      = $request->getParsedBody()['estado_tipo_subtipo_codigo'];
+        $val02                      = $request->getParsedBody()['tipo_codigo'];
+        $val03                      = $request->getParsedBody()['subtipo_codigo'];
+        $val04                      = $request->getParsedBody()['tipo_subtipo_valor'];
+        $val05                      = $request->getParsedBody()['tipo_subtipo_observacion'];
         
-        if (isset($val00) && isset($val01) && isset($val02) && isset($val03)) {
-            $sql                    = "UPDATE DOMCYS SET DOMCYS_COD = '$val01', DOMCYS_CAC = '$val02', DOMCYS_SUC = '$val03', MTCCSD_OBS = '".$val04."' WHERE MTCCSD_COD = '$val00'";
+        if (isset($val00) && isset($val01) && isset($val02) && isset($val03) && isset($val04)) {
+            $sql                    = "UPDATE DOMTYS SET DOMTYS_ESC = '$val01', DOMTYS_TIC = '$val02', DOMTYS_SUC = '$val03', DOMTYS_OBS = '".$val05."' WHERE DOMTYS_COD = '$val00'";
             if ($mysqli->query($sql) === TRUE) {
                 header("Content-Type: application/json; charset=utf-8");
                 $json               = json_encode(array('code' => 200, 'status' => 'ok', 'message' => 'Se actualizo con exito'), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
@@ -162,7 +246,7 @@
         $val00                      = $request->getAttribute('codigo');
         
         if (isset($val00)) {
-            $sql = "DELETE FROM DOMCYS WHERE DOMCYS_COD = '$val00'";
+            $sql = "DELETE FROM DOMTYS WHERE DOMTYS_COD = '$val00'";
             if ($mysqli->query($sql) === TRUE) {
                 header("Content-Type: application/json; charset=utf-8");
                 $json               = json_encode(array('code' => 200, 'status' => 'ok', 'message' => 'Se elimino con exito'), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
