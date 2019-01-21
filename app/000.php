2a6ea7cc3ebe11df23cbd37732acbb7036a2d0fe
CHRISTIAN E. ZELAYA SOSA <CEZS-SYSTEM@MacBook-Pro-de-CHRISTIAN.local>
2019-01-20 22:08:09 -0300

    bug 000.php

--- app/000.php
+++ app/000.php
@@ -1,15 +1,14 @@
 <?php
-	$app->post('/api/v1/000/user/{user}/pass/{pass}/uuid/{uuid}/ip/{ip}', function($request) {
+	$app->post('/api/v1/000', function($request) {
         require __DIR__.'/../src/connect.php';
-        
-        $val00                      = $request->getAttribute('user');
-        $val01                      = $request->getAttribute('pass');
-        $val02                      = $request->getAttribute('uuid');
-        $val03                      = $request->getAttribute('ip');
-        $val04                      = date("Y-m-d");
-        $val05                      = date("H:i:s");
 
-        $sql_00                     = "INSERT INTO USULOG (USULOG_EUC, USULOG_USC, USULOG_UUI, USULOG_DIP, USULOG_FEC, USULOG_HOR) VALUES (1, '$val01', '".$val02."', '".$val03."', '".$val04."', '".$val05."')";
+        $val01                      = strtoupper($request->getParsedBody()['usuario_var01']);
+        $val02                      = $request->getParsedBody()['usuario_var02'];
+        $val03                      = $request->getParsedBody()['usuario_var03'];
+        $val04                      = $request->getParsedBody()['usuario_var04'];
+        $val05                      = date("Y-m-d");
+        $val06                      = date("H:i:s");
+
         $sql_01                     = "SELECT
         b.DOMFIC_COD		    AS		estado_usuario_codigo,
         b.DOMFIC_NOM		    AS		estado_usuario_nombre,
@@ -27,7 +26,9 @@
             if ($query = $mysqli->query($sql_01)) {
                 while($row = $query->fetch_assoc()) {
                     if (password_verify($val01, $row['usuario_password'])) {
-                        if ($mysqli->query($sql_00) === TRUE) {
+                        $user   = $row['usuario_codigo'];
+                        $sql_02 = "INSERT INTO USULOG (USULOG_EUC, USULOG_USC, USULOG_UUI, USULOG_DIP, USULOG_FEC, USULOG_HOR) VALUES (1, , $user, '".$val03."', '".$val04."', '".$val05."', '".$val06."')";
+                        if ($mysqli->query($sql_02) === TRUE) {
                             header("Content-Type: application/json; charset=utf-8");
                             $json               = json_encode(array('code' => 200, 'status' => 'ok', 'message' => 'Acceso correcto. Bienvenido', 'codigo' => 0), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
                         } else {
