616596e7077891db0202884b37a8b5d2b80dbea4
CHRISTIAN E. ZELAYA SOSA <CEZS-SYSTEM@MacBook-Pro-de-CHRISTIAN.local>
2019-01-22 22:19:49 -0300

    add 1100.php

---
+++ app/1100.php
@@ -0,0 +1,248 @@
+<?php
+    $app->get('/api/v1/1100', function($request) {
+        require __DIR__.'/../src/connect.php';
+        
+		$sql                        = "SELECT
+		c.DOMFIC_COD		AS		subcategoria_ot_existencia_codigo,
+		c.DOMFIC_NOM		AS		subcategoria_ot_existencia_nombre,
+		b.ODTFIC_COD		AS		ot_codigo, 
+		b.ODTFIC_NRO		AS		ot_numero, 
+		b.ODTFIC_FIT		AS		ot_fecha_inicio_trabajo,
+        b.ODTFIC_FFT		AS		ot_fecha_final_trabajo,
+        b.ODTFIC_OBS		AS		ot_observacion,
+        a.ODTEXI_COD		AS		ot_existencia_codigo, 
+		a.ODTEXI_CAN		AS		ot_existencia_cantidad, 
+		a.ODTEXI_OBS		AS		ot_existencia_observacion
+		
+		FROM ODTEXI a
+		INNER JOIN ODTFIC b ON a.ODTEXI_ORC = b.ODTFIC_COD
+		INNER JOIN DOMFIC c ON a.ODTEXI_SUC = c.DOMFIC_COD
+		
+		ORDER BY a.ODTEXI_ORC";
+		
+        if ($query = $mysqli->query($sql)) {
+            while($row = $query->fetch_assoc()) {				
+                $detalle			= array(
+					'subcategoria_ot_existencia_codigo'	                    => $row['subcategoria_ot_existencia_codigo'],
+					'subcategoria_ot_existencia_nombre'	                    => $row['subcategoria_ot_existencia_nombre'],
+					'ot_codigo'		                                        => $row['ot_codigo'],
+					'ot_numero'		                                        => $row['ot_numero'],
+                    'ot_fecha_inicio_trabajo'	                            => $row['ot_fecha_inicio_trabajo'],
+                    'ot_fecha_inicio_trabajo_2'	                            => str_replace("-", "/", $row['ot_fecha_inicio_trabajo']),
+                    'ot_fecha_final_trabajo'	                            => $row['ot_fecha_final_trabajo'],
+                    'ot_fecha_final_trabajo_2'	                            => str_replace("-", "/", $row['ot_fecha_final_trabajo']),
+                    'ot_observacion'	                                    => $row['ot_observacion'],
+                    'ot_existencia_codigo'	                                => $row['ot_existencia_codigo'],
+                    'ot_existencia_cantidad'	                            => $row['ot_existencia_cantidad'],
+                    'ot_existencia_observacion'	                            => $row['ot_existencia_observacion']
+				);	
+                $result[]           = $detalle;
+            }
+			$query->free();
+        }
+        
+        $mysqli->close();
+        
+        if (isset($result)){
+            header("Content-Type: application/json; charset=utf-8");
+            $json                   = json_encode(array('code' => 200, 'status' => 'ok', 'message' => 'Consulta con exito', 'data' => $result), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+        } else {
+            header("Content-Type: application/json; charset=utf-8");
+            $json                   = json_encode(array('code' => 204, 'status' => 'ok', 'message' => 'No hay registros', 'data' => 'null'), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+        }
+        
+        return $json;
+    });
+
+    $app->get('/api/v1/1100/{codigo}', function($request) {
+        require __DIR__.'/../src/connect.php';
+        
+		$val00                      = $request->getAttribute('codigo');
+		$sql                        = "SELECT
+		c.DOMFIC_COD		AS		subcategoria_ot_existencia_codigo,
+		c.DOMFIC_NOM		AS		subcategoria_ot_existencia_nombre,
+		b.ODTFIC_COD		AS		ot_codigo, 
+		b.ODTFIC_NRO		AS		ot_numero, 
+		b.ODTFIC_FIT		AS		ot_fecha_inicio_trabajo,
+        b.ODTFIC_FFT		AS		ot_fecha_final_trabajo,
+        b.ODTFIC_OBS		AS		ot_observacion,
+        a.ODTEXI_COD		AS		ot_existencia_codigo, 
+		a.ODTEXI_CAN		AS		ot_existencia_cantidad, 
+		a.ODTEXI_OBS		AS		ot_existencia_observacion
+		
+		FROM ODTEXI a
+		INNER JOIN ODTFIC b ON a.ODTEXI_ORC = b.ODTFIC_COD
+		INNER JOIN DOMFIC c ON a.ODTEXI_SUC = c.DOMFIC_COD
+		
+		WHERE a.ODTEXI_COD = '$val00'
+		ORDER BY a.ODTEXI_COD";
+		
+        if ($query = $mysqli->query($sql)) {
+            while($row = $query->fetch_assoc()) {				
+                $detalle			= array(
+					'subcategoria_ot_existencia_codigo'	                    => $row['subcategoria_ot_existencia_codigo'],
+					'subcategoria_ot_existencia_nombre'	                    => $row['subcategoria_ot_existencia_nombre'],
+					'ot_codigo'		                                        => $row['ot_codigo'],
+					'ot_numero'		                                        => $row['ot_numero'],
+                    'ot_fecha_inicio_trabajo'	                            => $row['ot_fecha_inicio_trabajo'],
+                    'ot_fecha_inicio_trabajo_2'	                            => str_replace("-", "/", $row['ot_fecha_inicio_trabajo']),
+                    'ot_fecha_final_trabajo'	                            => $row['ot_fecha_final_trabajo'],
+                    'ot_fecha_final_trabajo_2'	                            => str_replace("-", "/", $row['ot_fecha_final_trabajo']),
+                    'ot_observacion'	                                    => $row['ot_observacion'],
+                    'ot_existencia_codigo'	                                => $row['ot_existencia_codigo'],
+                    'ot_existencia_cantidad'	                            => $row['ot_existencia_cantidad'],
+                    'ot_existencia_observacion'	                            => $row['ot_existencia_observacion']
+				);
+                $result[]           = $detalle;
+            }
+			$query->free();
+        }
+        
+        $mysqli->close();
+        
+        if (isset($result)){
+            header("Content-Type: application/json; charset=utf-8");
+            $json                   = json_encode(array('code' => 200, 'status' => 'ok', 'message' => 'Consulta con exito', 'data' => $result), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+        } else {
+            header("Content-Type: application/json; charset=utf-8");
+            $json                   = json_encode(array('code' => 204, 'status' => 'ok', 'message' => 'No hay registros', 'data' => 'null'), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+        }
+        
+        return $json;
+    });
+
+    $app->get('/api/v1/1100/ot/{codigo}', function($request) {
+        require __DIR__.'/../src/connect.php';
+        
+		$val00                      = $request->getAttribute('codigo');
+		$sql                        = "SELECT
+		c.DOMFIC_COD		AS		subcategoria_ot_existencia_codigo,
+		c.DOMFIC_NOM		AS		subcategoria_ot_existencia_nombre,
+		b.ODTFIC_COD		AS		ot_codigo, 
+		b.ODTFIC_NRO		AS		ot_numero, 
+		b.ODTFIC_FIT		AS		ot_fecha_inicio_trabajo,
+        b.ODTFIC_FFT		AS		ot_fecha_final_trabajo,
+        b.ODTFIC_OBS		AS		ot_observacion,
+        a.ODTEXI_COD		AS		ot_existencia_codigo, 
+		a.ODTEXI_CAN		AS		ot_existencia_cantidad, 
+		a.ODTEXI_OBS		AS		ot_existencia_observacion
+		
+		FROM ODTEXI a
+		INNER JOIN ODTFIC b ON a.ODTEXI_ORC = b.ODTFIC_COD
+		INNER JOIN DOMFIC c ON a.ODTEXI_SUC = c.DOMFIC_COD
+		
+		WHERE a.ODTEXI_ORC = '$val00'
+		ORDER BY a.ODTEXI_COD";
+		
+        if ($query = $mysqli->query($sql)) {
+            while($row = $query->fetch_assoc()) {				
+                $detalle			= array(
+					'subcategoria_ot_existencia_codigo'	                    => $row['subcategoria_ot_existencia_codigo'],
+					'subcategoria_ot_existencia_nombre'	                    => $row['subcategoria_ot_existencia_nombre'],
+					'ot_codigo'		                                        => $row['ot_codigo'],
+					'ot_numero'		                                        => $row['ot_numero'],
+                    'ot_fecha_inicio_trabajo'	                            => $row['ot_fecha_inicio_trabajo'],
+                    'ot_fecha_inicio_trabajo_2'	                            => str_replace("-", "/", $row['ot_fecha_inicio_trabajo']),
+                    'ot_fecha_final_trabajo'	                            => $row['ot_fecha_final_trabajo'],
+                    'ot_fecha_final_trabajo_2'	                            => str_replace("-", "/", $row['ot_fecha_final_trabajo']),
+                    'ot_observacion'	                                    => $row['ot_observacion'],
+                    'ot_existencia_codigo'	                                => $row['ot_existencia_codigo'],
+                    'ot_existencia_cantidad'	                            => $row['ot_existencia_cantidad'],
+                    'ot_existencia_observacion'	                            => $row['ot_existencia_observacion']
+				);
+                $result[]           = $detalle;
+            }
+			$query->free();
+        }
+        
+        $mysqli->close();
+        
+        if (isset($result)){
+            header("Content-Type: application/json; charset=utf-8");
+            $json                   = json_encode(array('code' => 200, 'status' => 'ok', 'message' => 'Consulta con exito', 'data' => $result), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+        } else {
+            header("Content-Type: application/json; charset=utf-8");
+            $json                   = json_encode(array('code' => 204, 'status' => 'ok', 'message' => 'No hay registros', 'data' => 'null'), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+        }
+        
+        return $json;
+    });
+
+	$app->post('/api/v1/1100', function($request) {
+        require __DIR__.'/../src/connect.php';
+
+		$val01                      = $request->getParsedBody()['subcategoria_ot_existencia_codigo'];
+        $val02                      = $request->getParsedBody()['ot_codigo'];
+        $val03                      = $request->getParsedBody()['ot_existencia_cantidad'];
+        $val04                      = $request->getParsedBody()['ot_existencia_observacion'];
+        
+        if (isset($val01) && isset($val02) && isset($val03)) {
+            $sql                    = "INSERT INTO DTEXI (ODTEXI_SUC, ODTEXI_ORC, ODTEXI_CAN, ODTEXI_OBS) VALUES ('$val01', '$val02', '$val03', '".$val04."')";
+            if ($mysqli->query($sql) === TRUE) {
+                header("Content-Type: application/json; charset=utf-8");
+                $json               = json_encode(array('code' => 200, 'status' => 'ok', 'message' => 'Se inserto con exito', 'codigo' => $mysqli->insert_id), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+            } else {
+                header("Content-Type: application/json; charset=utf-8");
+                $json               = json_encode(array('code' => 204, 'status' => 'ok', 'message' => 'No se pudo insertar', 'codigo' => 0), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+            }
+        } else {
+            header("Content-Type: application/json; charset=utf-8");
+            $json                   = json_encode(array('code' => 400, 'status' => 'error', 'message' => 'Verifique, algún campo esta vacio.', 'codigo' => 0), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+        }
+
+        $mysqli->close();
+        
+        return $json;
+    });
+
+	$app->put('/api/v1/1100/{codigo}', function($request) {
+        require __DIR__.'/../src/connect.php';
+        
+        $val00                      = $request->getAttribute('codigo');
+		$val01                      = $request->getParsedBody()['subcategoria_ot_existencia_codigo'];
+        $val02                      = $request->getParsedBody()['ot_codigo'];
+        $val03                      = $request->getParsedBody()['ot_existencia_cantidad'];
+        $val04                      = $request->getParsedBody()['ot_existencia_observacion'];
+        
+        if (isset($val00) && isset($val01) && isset($val02) && isset($val03)) {
+            $sql                    = "UPDATE ODTEXI SET ODTEXI_SUC = '$val01', ODTEXI_ORC = '$val02', ODTEXI_CAN = '$val03', ODTEXI_OBS = '".$val04."' WHERE ODTEXI_COD = '$val00'";
+            if ($mysqli->query($sql) === TRUE) {
+                header("Content-Type: application/json; charset=utf-8");
+                $json               = json_encode(array('code' => 200, 'status' => 'ok', 'message' => 'Se actualizo con exito'), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+            } else {
+                header("Content-Type: application/json; charset=utf-8");
+                $json               = json_encode(array('code' => 204, 'status' => 'ok', 'message' => 'No se pudo actualizar'), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+            }
+        } else {
+            header("Content-Type: application/json; charset=utf-8");
+            $json                   = json_encode(array('code' => 400, 'status' => 'error', 'message' => 'Verifique, algún campo esta vacio.'), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+        }
+
+        $mysqli->close();
+        
+        return $json;
+    });
+
+	$app->delete('/api/v1/1100/{codigo}', function($request) {
+        require __DIR__.'/../src/connect.php';
+        
+        $val00                      = $request->getAttribute('codigo');
+        
+        if (isset($val00)) {
+            $sql = "DELETE FROM ODTEXI WHERE ODTEXI_COD = '$val00'";
+            if ($mysqli->query($sql) === TRUE) {
+                header("Content-Type: application/json; charset=utf-8");
+                $json               = json_encode(array('code' => 200, 'status' => 'ok', 'message' => 'Se elimino con exito'), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+            } else {
+                header("Content-Type: application/json; charset=utf-8");
+                $json               = json_encode(array('code' => 204, 'status' => 'ok', 'message' => 'No se pudo eliminar'), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+            }
+        } else {
+            header("Content-Type: application/json; charset=utf-8");
+            $json                   = json_encode(array('code' => 400, 'status' => 'error', 'message' => 'Verifique, algún campo esta vacio.'), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+        }
+
+        $mysqli->close();
+        
+        return $json;
+    });
--- src/routes.php
+++ src/routes.php
@@ -10,3 +10,4 @@
 	require __DIR__.'/../app/800.php';                             //TABLA ESTFIC ESTABLECIMIENTO SECCIÓN
 	require __DIR__.'/../app/900.php';                             //TABLA ESTFIC ESTABLECIMIENTO POTRERO
 	require __DIR__.'/../app/1000.php';                            //TABLA ODTFIC ORDEN DE TRABAJO
+	require __DIR__.'/../app/1100.php';                            //TABLA ODTFIC ORDEN DE TRABAJO EXISTENCIA
