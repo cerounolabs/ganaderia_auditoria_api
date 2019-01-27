e1771202c90f3a91cc6a19818af85eadad4925a5
CHRISTIAN E. ZELAYA SOSA <CEZS-SYSTEM@MacBook-Pro-de-CHRISTIAN.local>
2019-01-27 01:12:11 -0300

    fix 900

--- app/900.php
+++ app/900.php
@@ -117,6 +117,66 @@
         return $json;
     });
 
+    $app->get('/api/v1/900/establecimiento/{codigo}', function($request) {
+        require __DIR__.'/../src/connect.php';
+        
+		$val00                      = $request->getAttribute('codigo');
+		$sql                        = "SELECT
+		d.DOMFIC_COD		AS		estado_potrero_codigo,
+		d.DOMFIC_NOM		AS		estado_potrero_nombre,
+		c.ESTFIC_COD		AS		establecimiento_codigo,
+		c.ESTFIC_NOM		AS		establecimiento_nombre,
+		c.ESTFIC_SIC		AS		establecimiento_sigor,
+        c.ESTFIC_OBS		AS		establecimiento_observacion,
+		b.ESTSEC_COD		AS		seccion_codigo, 
+		b.ESTSEC_NOM		AS		seccion_nombre, 
+		b.ESTSEC_OBS		AS		seccion_observacion,
+        a.ESTPOR_COD		AS		potrero_codigo, 
+		a.ESTPOR_NOM		AS		potrero_nombre, 
+		a.ESTPOR_OBS		AS		potrero_observacion
+		
+		FROM ESTPOR a
+        INNER JOIN ESTSEC b ON a.ESTPOR_SEC = b.ESTSEC_COD
+		INNER JOIN ESTFIC c ON b.ESTSEC_ESC = c.ESTFIC_COD
+		INNER JOIN DOMFIC d ON a.ESTPOR_EPC = d.DOMFIC_COD
+		
+		WHERE c.ESTFIC_COD = '$val00'
+		ORDER BY c.ESTFIC_NOM, b.ESTSEC_NOM, a.ESTPOT_NOM";
+		
+        if ($query = $mysqli->query($sql)) {
+            while($row = $query->fetch_assoc()) {				
+                $detalle			= array(
+					'estado_potrero_codigo'	                => $row['estado_potrero_codigo'],
+					'estado_potrero_nombre'	                => $row['estado_potrero_nombre'],
+					'establecimiento_codigo'	            => $row['establecimiento_codigo'],
+					'establecimiento_nombre'	            => $row['establecimiento_nombre'],
+                    'establecimiento_sigor'		            => $row['establecimiento_sigor'],
+                    'establecimiento_observacion'		    => $row['establecimiento_observacion'],
+					'seccion_codigo'		                => $row['seccion_codigo'],
+					'seccion_nombre'		                => $row['seccion_nombre'],
+                    'seccion_observacion'	                => $row['seccion_observacion'],
+                    'potrero_codigo'		                => $row['potrero_codigo'],
+					'potrero_nombre'		                => $row['potrero_nombre'],
+					'potrero_observacion'	                => $row['potrero_observacion']
+				);
+                $result[]           = $detalle;
+            }
+			$query->free();
+        }
+        
+        $mysqli->close();
+        
+        if (isset($result)){
+            header("Content-Type: application/json; charset=utf-8");
+            $json                   = json_encode(array('code' => 200, 'status' => 'ok', 'message' => 'Consulta con exito', 'data' => $result), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+        } else {
+            header("Content-Type: application/json; charset=utf-8");
+            $json                   = json_encode(array('code' => 204, 'status' => 'ok', 'message' => 'No hay registros', 'data' => 'null'), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+        }
+        
+        return $json;
+    });
+
 	$app->post('/api/v1/900', function($request) {
         require __DIR__.'/../src/connect.php';
 
