cb30a49d4a2ea199c9c5bea96f04c4bc05b09d74
CHRISTIAN E. ZELAYA SOSA <CEZS-SYSTEM@MacBook-Pro-de-CHRISTIAN.local>
2019-02-25 22:19:55 -0300

    fix OT Auditada

--- app/1000.php
+++ app/1000.php
@@ -22,7 +22,17 @@
 		ORDER BY a.ODTFIC_NRO DESC";
 		
         if ($query = $mysqli->query($sql)) {
-            while($row = $query->fetch_assoc()) {				
+            while($row = $query->fetch_assoc()) {
+                $dia1    = substr($row['ot_fecha_inicio_trabajo'], 8);
+                $mes1    = substr($row['ot_fecha_inicio_trabajo'], 5, -3);
+                $ano1    = substr($row['ot_fecha_inicio_trabajo'], 0, -6);
+                $fecha1  = $dia1.'/'.$mes1.'/'.$ano1;
+
+                $dia2    = substr($row['ot_fecha_final_trabajo'], 8);
+                $mes2    = substr($row['ot_fecha_final_trabajo'], 5, -3);
+                $ano2    = substr($row['ot_fecha_final_trabajo'], 0, -6);
+                $fecha2  = $dia2.'/'.$mes2.'/'.$ano2;
+
                 $detalle			= array(
 					'estado_ot_codigo'	                    => $row['estado_ot_codigo'],
 					'estado_ot_nombre'	                    => $row['estado_ot_nombre'],
@@ -33,9 +43,9 @@
 					'ot_codigo'		                        => $row['ot_codigo'],
 					'ot_numero'		                        => $row['ot_numero'],
                     'ot_fecha_inicio_trabajo'	            => $row['ot_fecha_inicio_trabajo'],
-                    'ot_fecha_inicio_trabajo_2'	            => str_replace("-", "/", $row['ot_fecha_inicio_trabajo']),
+                    'ot_fecha_inicio_trabajo_2'	            => $fecha1,
                     'ot_fecha_final_trabajo'	            => $row['ot_fecha_final_trabajo'],
-                    'ot_fecha_final_trabajo_2'	            => str_replace("-", "/", $row['ot_fecha_final_trabajo']),
+                    'ot_fecha_final_trabajo_2'	            => $fecha2,
                     'ot_observacion'	                    => $row['ot_observacion']
 				);	
                 $result[]           = $detalle;
@@ -97,7 +107,17 @@
 		ORDER BY a.ODTFIC_NRO DESC";
 		
         if ($query = $mysqli->query($sql)) {
-            while($row = $query->fetch_assoc()) {				
+            while($row = $query->fetch_assoc()) {
+                $dia1    = substr($row['ot_fecha_inicio_trabajo'], 8);
+                $mes1    = substr($row['ot_fecha_inicio_trabajo'], 5, -3);
+                $ano1    = substr($row['ot_fecha_inicio_trabajo'], 0, -6);
+                $fecha1  = $dia1.'/'.$mes1.'/'.$ano1;
+
+                $dia2    = substr($row['ot_fecha_final_trabajo'], 8);
+                $mes2    = substr($row['ot_fecha_final_trabajo'], 5, -3);
+                $ano2    = substr($row['ot_fecha_final_trabajo'], 0, -6);
+                $fecha2  = $dia2.'/'.$mes2.'/'.$ano2;
+
                 $detalle			= array(
 					'estado_ot_codigo'	                    => $row['estado_ot_codigo'],
 					'estado_ot_nombre'	                    => $row['estado_ot_nombre'],
@@ -108,9 +128,94 @@
 					'ot_codigo'		                        => $row['ot_codigo'],
 					'ot_numero'		                        => $row['ot_numero'],
                     'ot_fecha_inicio_trabajo'	            => $row['ot_fecha_inicio_trabajo'],
-                    'ot_fecha_inicio_trabajo_2'	            => str_replace("-", "/", $row['ot_fecha_inicio_trabajo']),
+                    'ot_fecha_inicio_trabajo_2'	            => $fecha1,
                     'ot_fecha_final_trabajo'	            => $row['ot_fecha_final_trabajo'],
-                    'ot_fecha_final_trabajo_2'	            => str_replace("-", "/", $row['ot_fecha_final_trabajo']),
+                    'ot_fecha_final_trabajo_2'	            => $fecha2,
+                    'ot_observacion'	                    => $row['ot_observacion']
+				);	
+                $result[]           = $detalle;
+            }
+			$query->free();
+        }
+        
+        $mysqli->close();
+        
+        if (isset($result)){
+            header("Content-Type: application/json; charset=utf-8");
+            $json       = json_encode(array('code' => 200, 'status' => 'ok', 'message' => 'Consulta con exito', 'data' => $result), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+        } else {
+            $detalle			= array(
+                'estado_ot_codigo'	                    => "",
+                'estado_ot_nombre'	                    => "",
+                'establecimiento_codigo'	            => "",
+                'establecimiento_nombre'	            => "",
+                'establecimiento_sigor'		            => "",
+                'establecimiento_observacion'		    => "",
+                'ot_codigo'		                        => "",
+                'ot_numero'		                        => "",
+                'ot_fecha_inicio_trabajo'	            => "",
+                'ot_fecha_inicio_trabajo_2'	            => "",
+                'ot_fecha_final_trabajo'	            => "",
+                'ot_fecha_final_trabajo_2'	            => "",
+                'ot_observacion'	                    => ""
+            );	
+            $result[]   = $detalle;
+            header("Content-Type: application/json; charset=utf-8");
+            $json       = json_encode(array('code' => 204, 'status' => 'ok', 'message' => 'No hay registros', 'data' => $detalle), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+        }
+        
+        return $json;
+    });
+
+    $app->get('/api/v1/1000/establecimiento/{codigo}', function($request) {
+        require __DIR__.'/../src/connect.php';
+        
+		$val00                      = $request->getAttribute('codigo');
+		$sql                        = "SELECT
+		c.DOMFIC_COD		AS		estado_ot_codigo,
+		c.DOMFIC_NOM		AS		estado_ot_nombre,
+		b.ESTFIC_COD		AS		establecimiento_codigo,
+		b.ESTFIC_NOM		AS		establecimiento_nombre,
+		b.ESTFIC_SIC		AS		establecimiento_sigor,
+        b.ESTFIC_OBS		AS		establecimiento_observacion,
+		a.ODTFIC_COD		AS		ot_codigo, 
+		a.ODTFIC_NRO		AS		ot_numero, 
+		a.ODTFIC_FIT		AS		ot_fecha_inicio_trabajo,
+        a.ODTFIC_FFT		AS		ot_fecha_final_trabajo,
+        a.ODTFIC_OBS		AS		ot_observacion
+		
+		FROM ODTFIC a
+		INNER JOIN ESTFIC b ON a.ODTFIC_ESC = b.ESTFIC_COD
+		INNER JOIN DOMFIC c ON a.ODTFIC_EOC = c.DOMFIC_COD
+		
+		WHERE a.ODTFIC_ESC = '$val00'
+		ORDER BY a.ODTFIC_NRO DESC";
+		
+        if ($query = $mysqli->query($sql)) {
+            while($row = $query->fetch_assoc()) {
+                $dia1    = substr($row['ot_fecha_inicio_trabajo'], 8);
+                $mes1    = substr($row['ot_fecha_inicio_trabajo'], 5, -3);
+                $ano1    = substr($row['ot_fecha_inicio_trabajo'], 0, -6);
+                $fecha1  = $dia1.'/'.$mes1.'/'.$ano1;
+
+                $dia2    = substr($row['ot_fecha_final_trabajo'], 8);
+                $mes2    = substr($row['ot_fecha_final_trabajo'], 5, -3);
+                $ano2    = substr($row['ot_fecha_final_trabajo'], 0, -6);
+                $fecha2  = $dia2.'/'.$mes2.'/'.$ano2;
+
+                $detalle			= array(
+					'estado_ot_codigo'	                    => $row['estado_ot_codigo'],
+					'estado_ot_nombre'	                    => $row['estado_ot_nombre'],
+					'establecimiento_codigo'	            => $row['establecimiento_codigo'],
+					'establecimiento_nombre'	            => $row['establecimiento_nombre'],
+                    'establecimiento_sigor'		            => $row['establecimiento_sigor'],
+                    'establecimiento_observacion'		    => $row['establecimiento_observacion'],
+					'ot_codigo'		                        => $row['ot_codigo'],
+					'ot_numero'		                        => $row['ot_numero'],
+                    'ot_fecha_inicio_trabajo'	            => $row['ot_fecha_inicio_trabajo'],
+                    'ot_fecha_inicio_trabajo_2'	            => $fecha1,
+                    'ot_fecha_final_trabajo'	            => $row['ot_fecha_final_trabajo'],
+                    'ot_fecha_final_trabajo_2'	            => $fecha2,
                     'ot_observacion'	                    => $row['ot_observacion']
 				);	
                 $result[]           = $detalle;
